a0cefbf00c3266820a80bd90b7e9d86e2038c44b
Bazzan <sebake01@gmail.com>
2020-09-08 18:17:11 +0200

    Init

---
+++ ShootEmUp/Assets/Scripts/PlayerInputManager.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class PlayerInputManager : MonoBehaviour
+{
+
+    public static PlayerInputAction inputActions;
+
+    private void Awake() => inputActions = new PlayerInputAction();
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+
+}
---
+++ ShootEmUp/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Vector2 wasdInput;
+    private Vector2 mouseInput;
+    private Rigidbody2D playerBody;
+
+    private Vector3 lookDirection;
+    private float degreesToRotate;
+
+
+
+    private void Awake()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        playerRotate();
+        Move();
+    }
+
+    private void playerRotate()
+    {
+
+        lookDirection = PlayerInputManager.inputActions.Player.Look.ReadValue<Vector2>();
+        lookDirection = lookDirection - Camera.main.WorldToScreenPoint(transform.position);
+
+        degreesToRotate = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        playerBody.MoveRotation(degreesToRotate - 90);
+
+    }
+
+    private void Move()
+    {
+        wasdInput = PlayerInputManager.inputActions.Player.Move.ReadValue<Vector2>();
+
+        playerBody.velocity = new Vector2(wasdInput.x * speed, wasdInput.y * speed);
+        playerBody.AddForce(wasdInput * speed * Time.fixedDeltaTime, ForceMode2D.Force);
+    }
+
+
+}
